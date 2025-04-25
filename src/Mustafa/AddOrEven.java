9d659895a0c53b67441edf74c3aa03788d44dc26
muus <nadireulsular@gmail.com>
2025-04-25 13:57:54 -0400

    created new code

--- src/Mustafa/AddOrEven.java
+++ src/Mustafa/AddOrEven.java
@@ -2,13 +2,14 @@ package Mustafa;
 
 public class AddOrEven {
     public static void main(String[] args) {
-        oddOrEven (20);
+        oddOrEven (21);
     }
 
     public static void oddOrEven(int num) {
         if (num % 2 == 0) {
             System.out.println (num + " is an even number");
         }
+
     }
 }
 
