4855e64e81993d53cdd06f23a55b78329f1e9004
virchenkoann <virchenko.ann@gmail.com>
2025-04-25 13:16:55 -0400

    Added a new package and oddOrEven method

---
+++ src/anna/Week01_OddOrEven.java
@@ -0,0 +1,16 @@
+package anna;
+
+public class Week01_OddOrEven {
+
+    public static void main(String[] args) {
+
+
+    }
+    public static void oddOrEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+    }
+}
