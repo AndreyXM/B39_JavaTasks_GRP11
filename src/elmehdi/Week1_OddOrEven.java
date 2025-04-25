dbb5a1cdb1af215ff1fb478bbe9dfbecad462d71
aboue <abouelouafaelmehdi8@gmail.com>
2025-04-25 12:21:13 -0500

    Added num on the oddOrEven method

--- src/elmehdi/Week1_OddOrEven.java
+++ src/elmehdi/Week1_OddOrEven.java
@@ -2,10 +2,10 @@ package elmehdi;
 
 public class Week1_OddOrEven {
     public static void main(String[] args) {
-
+        oddOrEven(4);
     }
 
     public static void oddOrEven ( int num ){
-        
+
     }
 }
