34d854b64b7949c65818bf5a26b2be914b3ee396
Sevinch13 <sevinchmirsalova@gmail.com>
2025-05-02 10:29:33 -0600

    Created OddOrEven Class Edited

--- src/sevinch/Week1_OddOrEven.java
+++ src/sevinch/Week1_OddOrEven.java
@@ -1,4 +1,15 @@
 package sevinch;
 
 public class Week1_OddOrEven {
+    public static void main(String[] args) {
+        oddOrEven(9);
+
+    }
+    public static void oddOrEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+    }
 }
