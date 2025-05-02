4ec3b54751c06b73bb4fa0974fa7478925ae7cd9
VictoriGnatiuc <victoriagnatiuc@gmail.com>
2025-05-02 12:39:20 -0400

    Week1_OddOrEven class odd or even conditions added

--- src/victoria/Week1_OddOrEven.java
+++ src/victoria/Week1_OddOrEven.java
@@ -6,7 +6,14 @@ public class Week1_OddOrEven {
 
     }
 
-    public static void oddOrEven(int num){
+    public static void oddOrEven(int num) {
+        if (num == 0) {
+            System.out.println("Although " + num + " is accepted an even number but it's a special number.");
+        } else if (num % 2 == 0) {
+            System.out.println(num + "is even number.");
+        } else {
+            System.out.println(num + "is odd number.");
+        }
 
     }
 }
