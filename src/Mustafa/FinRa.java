836f757ffa7ca6c8a42cbdd4c44fae728a3f2599
muus <nadireulsular@gmail.com>
2025-05-10 11:27:10 -0400

    FINRA project is modified

--- src/Mustafa/FinRa.java
+++ src/Mustafa/FinRa.java
@@ -7,15 +7,29 @@ public class FinRa {
     public static void finraExample(int n) {
         for (int i = 1; i <= n; i++) {
             if (i % 3 ==0 && i % 5 == 0) {  // we can write, i % 15 == 0  as well
-                System.out.println ("FINRA");
+                System.out.print ("FINRA ");
             } else if (i % 3 == 0) {
-                System.out.println ("FIN");
+                System.out.print ("FIN ");
             } else if (i % 5 == 0) {
-                System.out.println ("RA");
+                System.out.print ("RA ");
             } else {
-                System.out.println (i);
+                System.out.print ( i + " " );
             }
         }
 
     }
 }
+/*
+  public static void main(String[] args) {
+        finra(30);
+    }
+
+    public static void finra(int num){
+        for(int i=1; i <= num; i++){
+            if (i % 15 == 0) System.out.print("FINRA ");
+            else if (i % 5 == 0) System.out.print("RA ");
+            else if (i % 3 == 0) System.out.print("FIN ");
+            else System.out.print(i + " ");
+        }
+    }
+ */
