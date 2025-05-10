cd69d8460a4976080d671b81a4d1587acb463438
muus <nadireulsular@gmail.com>
2025-05-09 20:46:31 -0400

    Swap 2 numbers project added

---
+++ src/Mustafa/SwapTwoNumbers.java
@@ -0,0 +1,31 @@
+package Mustafa;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 10;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println ("a = " + a);
+        System.out.println ("b = " + b);
+
+
+        System.out.println ("*********");
+
+        int c = 10;
+        int d = 15;
+        int swap = 0;
+
+        swap = c;
+        c = d;
+        d = swap;
+
+        System.out.println ("c = " + c);
+        System.out.println ("d = " + d);
+
+
+    }
+}
