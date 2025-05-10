2a9d788945dcc5b6050e784c2195e7234092e712
muus <nadireulsular@gmail.com>
2025-05-10 10:41:16 -0400

    Swap 2 numbers project modified

--- src/Mustafa/SwapTwoNumbers.java
+++ src/Mustafa/SwapTwoNumbers.java
@@ -4,28 +4,36 @@ public class SwapTwoNumbers {
     public static void main(String[] args) {
         int a = 4;
         int b = 10;
+        System.out.println ("Before swaping");
+        System.out.println ("c = " + a);
+        System.out.println ("d = " + b);
 
         a = a + b;
         b = a - b;
         a = a - b;
 
-        System.out.println ("a = " + a);
-        System.out.println ("b = " + b);
+        System.out.println ("Before swaping");
+        System.out.println ("c = " + a);
+        System.out.println ("d = " + b);
 
 
         System.out.println ("*********");
 
-        int c = 10;
-        int d = 15;
-        int swap;
-
-        swap = c;
-        c = d;
-        d = swap;
-
+        int c = 7;
+        int d = 5;
+        System.out.println ("Before swaping");
         System.out.println ("c = " + c);
         System.out.println ("d = " + d);
 
 
+        c = c * d; // 35
+        d = c / d; //  7
+        c = c / d; //  5
+
+        System.out.println ("After swaping");
+        System.out.println ("c = " + c);
+        System.out.println ("d = " + d);
+
     }
 }
+
