fdafbb5613822d989dd437719767c05365f472f3
muus <nadireulsular@gmail.com>
2025-07-05 16:51:37 -0400

    Password Validation code is created.

---
+++ src/Mustafa/PasswordValidation.java
@@ -0,0 +1,32 @@
+package Mustafa;
+
+public class PasswordValidation {
+    public static void main(String[] args) {
+        System.out.println (validatePassword ("PASS"));
+        System.out.println (validatePassword ("Password123@"));
+        System.out.println (validatePassword ("Ad1@"));
+        System.out.println (validatePassword (null));
+    }
+
+    public static boolean validatePassword(String password) {
+
+        return (password != null
+                && !password.contains (" ")
+                && password.length () >= 6
+                && password.matches (".*[a-z].*")
+                && password.matches (".*[A-Z].*")
+                && password.matches (".*[@#$%&*].*"));
+
+    }
+}
+
+/*
+Write a return method that can verify if a password is valid or not
+- Password must be at least having 6 characters and should not contain space
+- Password should at least contain one upper case letter
+- Password should at least contain one lowercase letter
+- Password should at least contain one special character
+- Password should at least contain a digit
+
+If all requirements above are met, the method returns true, otherwise false.
+ */
